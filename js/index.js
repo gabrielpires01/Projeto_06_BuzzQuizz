2053aa71aa6d244317357463863ad4e6fd258f89
Gabriel Pires <gabriel.pires@twistsystems.com>
2021-08-11 16:40:20 -0300

    Quizz Creation layout first page

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const url = 'https://mock-api.bootcamp.respondeai.com.br/api/v3/buzzquizz/quizzes';
+
+const createQuizz = () => {
+    document.querySelector('.container').classList.add('hidden')
+    document.querySelector('.quizzCreation').classList.remove('hidden')
+}
+
+const questionsPage = () => {
+     
+}
